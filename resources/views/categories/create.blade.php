361865c9fa919db468ca66b973229eb1d5f4c0a4
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2024-08-08 02:06:51 +0300

    done created categories page

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section("content")
+
+
+<div class="card card-default">
+    <div class="card-header">create Category</div>
+    <div class="card-body">
+        <form action="{{ route('categories.store') }}" method="POST">
+            @csrf
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" id="name" class="form-control" name="name">
+</div>
+
+<div class="form-group">
+    <button class="btn btn-success">Add Category </button>
+</div>
+</form>
+</div>
+                
+</div>
+@endsection
